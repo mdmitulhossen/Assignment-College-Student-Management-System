448cae3f9a1b228fcfdbfe61f25ff84057d8083b
mdmitulhossen <md.mitul.hossen99@gmail.com>
2025-12-26 00:31:02 +0600

    feat: project setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="text-4xl font-bold text-center py-10 font-sans">
-      Coming soon...
+      <p className="text-cyan-800">Coming soon...</p>
     </div>
   );
 }
